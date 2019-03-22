68410ab15970d2a87954962dba335dd6b64c02ed
niaobulashi <hulang6666@qq.com>
2019-03-22 15:20:46 +0800

    调整application位置

--- src/main/java/com/niaobulashi/JavademoApplication.java
+++ src/main/java/com/niaobulashi/JavademoApplication.java
@@ -1,4 +1,4 @@
-package com.niaobulashi.javademo;
+package com.niaobulashi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
