9a0c7fa1c5d47c4bebc500ec6b1ae1852f855007
hulang <hulang6666@qq.com>
2021-09-01 16:09:44 +0800

    123

---
+++ src/main/java/com/niaobulashi/concurrent/no01/TestThreadPool.java
@@ -0,0 +1,17 @@
+package com.niaobulashi.concurrent.no01;
+
+
+/**
+ * @version V1.0
+ * @Description: 测试代码
+ * @author: hulang
+ * @date: 2021/08/26
+ * @Copyright: 长安国际信托股份有限公司 http://www.caitc.cn
+ */
+public class TestThreadPool {
+    
+    public static void main(String[] args) {
+        // 创建3个线程的线程池
+        ThreadPool t = ThreadPool.getThreadPool();
+    }
+}
